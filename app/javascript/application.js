6aeacbedc15cef5187cd2b322f9ffdc339f83ce3
devdk <deividns07@gmail.com>
2023-04-07 09:08:54 -0300

    Aula 03

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// Adicione este código:
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.querySelectorAll("[data-confirm]").forEach(function(element) {
+    element.addEventListener("click", function(e) {
+      if (!confirm(element.getAttribute("data-confirm"))) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    });
+  });
+});
